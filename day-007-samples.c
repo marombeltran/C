08946777691424c24942834dba612d1e7b3f0cd0
Marlon Beltran <mbeltran@marencoventiladores.com>
2023-06-09 16:38:39 -0600

    Change - samples 6

--- day-007-samples.c
+++ day-007-samples.c
@@ -11,17 +11,41 @@ main (int argc, char *argv[])
     /* • A separator separates tokens. White space (see next section) is a separator, but it is not a
      *   token.
      */
-    int a, b;
-    a = 3; // white space as element's separator
-    b = 4;
+        int a, b;
+        a = 3; // white space as element's separator
+        b = 4;
  
     // Remenber: white space is not a token
-    a + b;
+        a + b;
 
     /* • The other separators are all single-character tokens themselves:
      * ( ) [ ] { } ; , . :
      */
+
+    /* 1.6 White Space
+     * • White space is the collective term used for several characters: the space character, the tab
+     *   character, the newline character, the vertical tab character, and the form-feed character.
+     *
+     * • White space is ignored (outside of string and character constants), and is therefore optional,
+     *   except when it is used to separate tokens.
+     */
+
+        // that ..
+        printf ( "hello, world\n" );
+
+        // ..is the same this. It doesn't matter.
+        printf("hello, world\n");
     
+    /* • Although you must use white space to separate many tokens, no white space is required
+     *   between operators and operands
+     */
+        int x = 1;
+        x++;
+        x ++ ; 
+
+        x
+            ++
+                ;
 
     exit (EXIT_SUCCESS);
  }
