d0413f37546525d78ef15d71c5a95bad447fe44e
Marom Beltran <mrmbeltran@proton.me>
2023-06-10 20:26:03 -0600

    Change - day 8

---
+++ day-008-samples.c
@@ -0,0 +1,25 @@
+/* day-007-samples -- source code for day-007
+ * 2023 Free Software Foundation, Inc.
+ */ 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main (int argc, char *argv[])
+{
+
+   /* 2 Data Types
+    *
+    * 2.1.1 Integer Types
+    * • The integer data types range in size from at least 8 bits to at least 32 bits.
+    *
+    * • The C99 standard extends this range to include integer sizes of at least 64 bits.
+    *
+    * • You should use integer types for storing whole number values (and the char data 
+    *   type for storing characters). The sizes and ranges listed for these types are minimums; 
+    *   depending on your computer platform, these sizes and ranges may be larger.
+    */ 
+
+   exit (EXIT_SUCCESS);
+}
