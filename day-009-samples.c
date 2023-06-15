4dab0dd13d16fce3bdae39619fe010aea2e14e31
Marom Beltran <mrmbeltran@proton.me>
2023-06-14 18:55:11 -0600

    Change - day 9

---
+++ day-009-samples.c
@@ -0,0 +1,24 @@
+/* day-009-samples -- source code for day-009
+ * 2023 Free Software Foundation, Inc.
+ */ 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main (int argc, char *argv[])
+{
+
+   /* • Expressions combine variables and constants to produce new values.
+    *
+    * • The type of an object determines the set of values it can have and what operations
+    *   can be performed on it.
+    *
+    * • Don't begin variable names with underscore, however, since library rou-
+    *   tines often use such names.
+    *
+    * • At least the first 31 characters of an internal name are significant.
+    */
+
+   exit (EXIT_SUCCESS);
+}
