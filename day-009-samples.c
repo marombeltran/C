54e22c9a040f9b123049a4d7d442d31b610916bd
Marom Beltran <mrmbeltran@proton.me>
2023-06-14 19:27:41 -0600

    Change - day 9

--- day-009-samples.c
+++ day-009-samples.c
@@ -4,12 +4,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 int
 main (int argc, char *argv[])
 {
 
-   /* • Expressions combine variables and constants to produce new values.
+   /* 3.1 Expressions
+    *
+    * • Expressions combine variables and constants to produce new values.
     *
     * • The type of an object determines the set of values it can have and what operations
     *   can be performed on it.
@@ -18,6 +21,18 @@ main (int argc, char *argv[])
     *   tines often use such names.
     *
     * • At least the first 31 characters of an internal name are significant.
+    *
+    * An expression consists of at least one operand and zero or more operators. Operands are
+    * typed objects such as constants, variables, and function calls that return values.
+    *
+    * Innermost expressions are evaluated first.
+    */
+
+      printf ("INT MAX -> %d", INT_MAX);
+
+   /* 4 Statements
+    * You write statements to cause actions and to control flow within your programs. You can
+    * also write statements that do not do anything at all, or do things that are uselessly trivial.
     */
 
    exit (EXIT_SUCCESS);
