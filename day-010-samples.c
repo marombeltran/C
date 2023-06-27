afb252f2ebdb05851e2e447f83371ec1525d695c
Marlon Beltran <mrmbeltran@proton.me>
2023-06-27 16:20:52 -0600

    Change - Day 10 - entry

--- day-010-samples.c
+++ day-010-samples.c
@@ -4,11 +4,38 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 int
 main (int argc, char *argv[])
 {
+   // An 'expression' is a combination of values, variables, operators, and function calls that evaluates to a single value. 
+
+   /* A 'statement' is a complete line or block of code that performs a specific action or task. 
+    * It represents a complete instruction in a program and can consist of expressions, 
+    * keywords, variables, and control flow constructs. Statements are executed 
+    * sequentially unless altered by control flow constructs such as loops 
+    * or conditional statements.
+    */
+
+      int x;
+
+      x = 5; // assigment statement
+
+      { // block statement 
+
+         if (x <= 5) // conditional statement
+
+         // block statement 
+         {
+            printf ("x is less than or equal to -> %d\n", x); // call function statement
+         }
+         else // statement
+            puts ("i will never show myself");
+      }
+
+   /* They do not produce a value themselves but instead perform actions or cause side effects.
+    */
+
 
    exit (EXIT_SUCCESS);
 }
