0b5c1ebdf9c9a2c00c1c9f3a422f5d886a5ef566
Marlon Beltran <mrmbeltran@proton.me>
2023-06-29 10:16:32 -0600

    Change - day 012

---
+++ day-012-samples.c
@@ -0,0 +1,14 @@
+/* day-012-samples -- source code for day-012
+ * 2023 Free Software Foundation, Inc.
+ */ 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int
+main (int argc, char *argv[])
+{
+
+   exit (EXIT_SUCCESS);
+}
