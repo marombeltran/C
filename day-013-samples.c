d42ec672420794e66267d43fac94d083a08e2fcf
Marom Beltran <mrmbeltran@proton.me>
2023-06-30 19:01:30 -0600

    Add - day 013

---
+++ day-013-samples.c
@@ -0,0 +1,14 @@
+/* day-013-samples -- source code for day-013
+ * 2023 Free Software Foundation, Inc.
+ */ 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int
+main (int argc, char *argv[])
+{
+
+   exit (EXIT_SUCCESS);
+}
