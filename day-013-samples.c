b5aecb1e134a6bad7a1f2c714fd7bd2f91cd8f0e
Marom Beltran <mrmbeltran@proton.me>
2023-07-02 20:29:43 -0600

    Change - enums

--- day-013-samples.c
+++ day-013-samples.c
@@ -8,11 +8,20 @@
 
 int
 main (int argc, char *argv[])
-{
+ {
 
    /* Examples and challenges using enumerations.
     * --------------------------------------------------------------------------------------------
     */
 
+   // definition syntax
+   // enum [identifier] { enumerator-list }
+
+   /* They usually are used when we have a fixed set of values that a variable can take on, and you want
+    * to make your code more readable and self-explatory.
+    */
+      enum toggle { OFF, ON, NO = 0, YES };
+
+
    exit (EXIT_SUCCESS);
-}
+ }
